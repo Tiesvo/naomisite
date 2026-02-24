d4433469e4e88cf64112fd4474f9d89c5edca19c
Ties <ties@vanoldenborgh.com>
2026-02-24 15:04:49 +0400

    Initial commit

--- app/_data/content.ts
+++ app/_data/content.ts
@@ -3,8 +3,8 @@ export const content = {
   title: "Model & Creator",
   tagline: "Welcome.. find my spicy links here. Will you send me a message babe?",
   ctas: [
-    { label: "Onlyfans", href: "https://onlyfans.com/naomivanstraatten", brand: "onlyfans" },
-    { label: "F2F", href: "https://f2f.com/naomivanstraatten", brand: "f2f" },
+    { label: "Onlyfans", href: "https://onlyfans.com/naomivanstraatten/c1", brand: "onlyfans" },
+    { label: "F2F", href: "https://f2f.com/s/q71HJl", brand: "f2f" },
   ],
   gallery: Array.from({ length: 19 }).map((_, i) =>
     `/gallery/${String(i + 1).padStart(3, "0")}.jpg`
